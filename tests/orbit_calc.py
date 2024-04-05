af595dbfc1a6cd44dd07c4c854c560eaf58d47d1
ABema <a.maya3141592@icloud.com>
2024-04-05 12:55:34 +0900

    2インパルスホーマン移行を描画するコードを作成した
    Signed-off-by: ABema <a.maya3141592@icloud.com>

--- tests/orbit_calc.py
+++ tests/orbit_calc.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Constants
 GM = 398600.4354360959 # Earth's gravitational constant
-R = 6371000 # Earth radius
+R = 6378.137 # Earth radius
 T = 86164.1 # Sidereal day
 w = 2 * math.pi / T # Angular velocity
 h = R + 35800 * 1000 # Altitude
@@ -11,16 +11,20 @@ v = math.sqrt(GM / h) # Orbital velocity
 
 
 # Function
-# 位置を引数ととった円軌道の速度
+# 任意の位置で円軌道が成立する時の速さ
 def v_circular(x):
     r = np.linalg.norm(x[0:3])
     return math.sqrt(GM / r)
 
-# 半径を引数ととった円軌道の軌道周期
+# 任意の位置における円軌道が成立するときの軌道周期
 def T_circular(x):
     r = np.linalg.norm(x[0:3])
     v = v_circular(x[0:3])
     return 2 * math.pi * r / v
+
+def T_owbow(x):
+    r = np.linalg.norm(x[0:3])
+    return 7 * math.pi * np.sqrt(r**3 / GM)
 
 # 2インパルスホーマン移行を計算する関数
 def hohmann(x1, x2):
@@ -33,3 +37,23 @@ def hohmann(x1, x2):
     dv1 = v - v1
     dv2 = v2 - v
     return dv1, dv2
+
+# 2インパルスホーマン移行において，遷移開始高度と軌道半径の差分をインプットとしたとき，終端位置の座標を返す関数
+def hohmann_pos(r1, dr):
+    # r1 = np.linalg.norm(x1[0:3])  # 遷移開始位置の軌道半径
+    r2 = r1 + dr  # 遷移終了位置の軌道半径
+    a = (r1 + r2) / 2  # 遷移軌道の半長軸
+    v1 = np.sqrt(GM / r1)  # 遷移開始位置の速さ
+    v2 = np.sqrt(GM / r2)  # 遷移終了位置の速さ
+    vp = np.sqrt(2*GM*(1/r1 - 1/(r1+r2)))
+    va = np.sqrt(2*GM*(1/r2 - 1/(r1+r2)))
+    dv1 = vp - v1  # 遷移開始位置での速度変化
+    dv2 = v2 - va  # 遷移終了位置での速度変化
+    x1 = [r1, 0, 0, 0, v1, 0]  # 遷移開始位置の座標
+    x2 = [-r2, 0, 0, 0, -v2, 0]  # 遷移終了位置の座標
+    tr = [r1, 0, 0, 0, vp, 0]  # 遷移軌道出発時のカルテシアン要素
+    return x1, x2, tr
+
+
+if __name__ == '__main__':
+    print(hohmann_pos(R + 200, 35586))
---
+++ tests/two_body.py
@@ -0,0 +1,57 @@
+# Import Python Modules
+import numpy as np # 数値計算ライブラリ
+from scipy.integrate import odeint # 常微分方程式を解くライブラリ
+import matplotlib.pyplot as plt # 描画ライブラリ
+import orbit_calc as oc # 自作ライブラリ
+
+r_E = 6371 # 地球の半径, km
+
+# 二体問題の運動方程式
+def func(x, t):
+    GM = 398600.4354360959 # 地球の重力定数, km3/s2
+    r = np.linalg.norm(x[0:3])
+    dxdt = [x[3],
+            x[4],
+            x[5],
+            -GM*x[0]/(r**3),
+            -GM*x[1]/(r**3),
+            -GM*x[2]/(r**3)]
+    return dxdt
+
+def draw_hohman_orbit(x1, x2, tr):
+    # 微分方程式の初期条件
+    t1  = np.linspace(0, oc.T_circular(x1), 1000) # 1日分 軌道伝播
+    sol1 = odeint(func, x1, t1)
+
+    t2 = np.linspace(0, oc.T_circular(x2), 1000) # 1日分 軌道伝播
+    sol2 = odeint(func, x2, t2)
+
+    ttr = np.linspace(0, oc.T_owbow(tr), 1000)
+    soltr = odeint(func, tr, ttr)
+
+    # 描画
+    plt.plot(soltr[:, 0], soltr[:, 1], 'k')
+    plt.plot(sol1[:, 0], sol1[:, 1], 'b')
+    plt.plot(sol2[:, 0], sol2[:, 1], 'r')
+    plt.grid()  # 格子をつける
+    plt.gca().set_aspect('equal')  # グラフのアスペクト比を揃える
+    plt.show()
+
+if __name__ == '__main__':
+    # 地球の輪郭
+    # x_E = [r_E, 0, 0, 0, 7.9, 0] # 位置(x,y,z)＋速度(vx,vy,vz)
+    # t_E  = np.linspace(0, 5100, 100) # 1日分 軌道伝播
+    # solE = odeint(func, x_E, t_E)
+
+    # 微分方程式の初期条件
+    x0 = [r_E+1000, 0, 0] # 位置(x,y,z)
+    x0 = [r_E+1000, 0, 0, 0, oc.v_circular(x0), 0] # 位置(x,y,z)＋速度(vx,vy,vz)
+    t0  = np.linspace(0, oc.T_circular(x0), 1000) # 1日分 軌道伝播
+    sol0 = odeint(func, x0, t0)
+
+    # 描画
+    # plt.plot(solE[:, 0],solE[:, 1],'k')
+    plt.plot(sol0[:, 0],sol0[:, 1], 'b')
+    plt.grid() # 格子をつける
+    plt.gca().set_aspect('equal') # グラフのアスペクト比を揃える
+    plt.show()
