303cf5494f6b5d10f5b1a80b4392bd3b67aef054
ABema <a.maya3141592@icloud.com>
2024-05-14 18:25:00 +0900

    3体問題関数の実装テスト
    Signed-off-by: ABema <a.maya3141592@icloud.com>

--- tests/test2.py
+++ tests/test2.py
@@ -1,10 +1,16 @@
 import two_body as tb
-import orbit_calc as oc
 import numpy as np
 import matplotlib.pyplot as plt
-import ode
 
-R = 6378.137 # Earth radius
-x1 = [R+36000, 0, 0, 0, 3.0668882673431845, 0]
+R = 6371  # 地球の半径, km
+x1 = [R+36000, 0, 0, 0, 3.0668882673431845/1.414, 3.0668882673431845/1.414]
+y1 = [384400, 0, 0, 0, 1.022, 0]
 
-
+solx, soly = tb.MoonEarthSat(x1, y1, 10000, 100000)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(solx[:, 0], solx[:, 1], solx[:, 2], 'b')
+ax.plot(soly[:, 0], soly[:, 1], soly[:, 2], 'r')
+plt.grid()  # 格子をつける
+plt.gca().set_aspect('equal')  # グラフのアスペクト比を揃える
+plt.show()
