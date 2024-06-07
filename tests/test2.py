8a8ab1a2552c5385aeef6304561a5b759941574a
ABema <a.maya3141592@icloud.com>
2024-06-07 17:41:27 +0900

    月周回軌道の描画が可能に
    Signed-off-by: ABema <a.maya3141592@icloud.com>

--- tests/test2.py
+++ tests/test2.py
@@ -1,42 +1,23 @@
 import two_body as tb
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
-R = 6371  # 地球の半径, km
-x1 = [R + 36000, 0, 0, 0, 3.0668882673431845 / 1.114, 3.0668882673431845]
+# x1 = [R+200, 0, 0, 0, 7.784261686425335, 0]
+# x1 = [384400+5000, 0, 0, 0, 1.022+0.85/1.414, 0.85+0.2]  # naname
+x1 = [384400+3000, 0, 0, 0, 1.022+1.02/1.414, 1.02/1.414]  # 3000km
 y1 = [384400, 0, 0, 0, 1.022, 0]
 
-solx, soly = tb.MoonEarthSat(x1, y1, 60, 10)
 
+# 描画
+# plt.plot(solE[:, 0],solE[:, 1],'k')
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# 軌道の範囲を設定
-ax.set_xlim([min(solx[:, 0]), max(solx[:, 0])])
-ax.set_ylim([min(solx[:, 1]), max(solx[:, 1])])
-ax.set_zlim([min(solx[:, 2]), max(solx[:, 2])])
-
-# 軌道の描画
-line_solx, = ax.plot([], [], [], 'b')
-line_soly, = ax.plot([], [], [], 'r')
-
-def init():
-    line_solx.set_data([], [])
-    line_solx.set_3d_properties([])
-    line_soly.set_data([], [])
-    line_soly.set_3d_properties([])
-    return line_solx, line_soly
-
-def update(frame):
-    line_solx.set_data(solx[:frame, 0], solx[:frame, 1])
-    line_solx.set_3d_properties(solx[:frame, 2])
-    line_soly.set_data(soly[:frame, 0], soly[:frame, 1])
-    line_soly.set_3d_properties(soly[:frame, 2])
-    return line_solx, line_soly
-
-# 100倍速くするためにintervalを小さくする
-ani = FuncAnimation(fig, update, frames=len(solx), init_func=init, blit=True, interval=1)
-
+solx, soly = tb.MoonEarthSat(x1, y1, 1, 100)
+# fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(solx[:, 0], solx[:, 1], solx[:, 2], 'b')
+ax.plot(soly[:, 0], soly[:, 1], soly[:, 2], 'r')
 plt.grid()  # 格子をつける
+plt.gca().set_aspect('equal')  # グラフのアスペクト比を揃える
 plt.show()
